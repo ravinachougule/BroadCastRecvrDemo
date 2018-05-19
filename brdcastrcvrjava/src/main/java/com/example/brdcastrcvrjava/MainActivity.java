ac99c22a9f9eaba2462c879815d0ecfec58fc63a
ravinachougule <ravinachougule7@gmail.com>
2018-05-19 19:09:45 +0530

    BroadCastRcvr

---
+++ brdcastrcvrjava/src/main/java/com/example/brdcastrcvrjava/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.brdcastrcvrjava;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BroadcastReceiver receiver=new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context,"AirPlane Mode Changed",Toast.LENGTH_SHORT).show();
+            if(intent.getBooleanExtra("state",false)){
+                findViewById(R.id.imgPlane).setVisibility(View.VISIBLE);
+            }
+            else{
+                findViewById(R.id.imgPlane).setVisibility(View.GONE);
+            }
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter=new IntentFilter();
+        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        registerReceiver(receiver,filter);
+    }
+
+    @Override
+    protected void onStop() {
+        unregisterReceiver(receiver);
+        super.onStop();
+    }
+}
